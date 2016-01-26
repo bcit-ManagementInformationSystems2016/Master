45160f8f138425b6258d8e398ff80824f7fa1e85
soran <soran0621@gmail.com>
2016-01-26 14:55:51 -0800

    First commit

---
+++ JavaSource/com/corejsf/UserBean.java
@@ -0,0 +1,20 @@
+package com.corejsf;
+
+import java.io.Serializable;
+import javax.inject.Named; 
+   // or import javax.faces.bean.ManagedBean;
+import javax.enterprise.context.SessionScoped; 
+   // or import javax.faces.bean.SessionScoped;
+
+@Named("user") // or @ManagedBean(name="user")
+@SessionScoped
+public class UserBean implements Serializable {
+   private String name;
+   private String password;
+
+   public String getName() { return name; }   
+   public void setName(String newValue) { name = newValue; }
+
+   public String getPassword() { return password; }
+   public void setPassword(String newValue) { password = newValue; }   
+}
