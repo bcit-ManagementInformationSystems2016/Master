0d790238d794d6ce184f9fcb37f10284707e1c15
Christopher Bow <chris@christopherbow.com>
2016-03-19 17:11:19 -0700

    finished the functionality of modifying timesheet validator

--- JavaSource/ca/bcit/infosys/controllers/SupervisorController.java
+++ JavaSource/ca/bcit/infosys/controllers/SupervisorController.java
@@ -9,6 +9,7 @@ import javax.inject.Named;
 
 import ca.bcit.infosys.managers.EmployeeManager;
 import ca.bcit.infosys.models.Employee;
+import ca.bcit.infosys.models.ProjectEmployees;
 
 @Named("supervisorController")
 @SessionScoped
@@ -20,7 +21,7 @@ public class SupervisorController implements Serializable {
 	//local variables
 	private Employee viewableEmployee;
 	private Employee timesheetValidator;
-	private Employee newTimesheetValidator;
+	private String newTimesheetValidator;
 	
 	// GETTERS AND SETTERS
 	public Employee getViewableEmployee() {
@@ -35,10 +36,10 @@ public class SupervisorController implements Serializable {
 	public void setTimesheetValidator(Employee timesheetValidator) {
 		this.timesheetValidator = timesheetValidator;
 	}
-	public Employee getNewTimesheetValidator() {
+	public String getNewTimesheetValidator() {
 		return newTimesheetValidator;
 	}
-	public void setNewTimesheetValidator(Employee newTimesheetValidator) {
+	public void setNewTimesheetValidator(String newTimesheetValidator) {
 		this.newTimesheetValidator = newTimesheetValidator;
 	}
 	
@@ -62,6 +63,14 @@ public class SupervisorController implements Serializable {
 		}
 		System.out.println("Employee using: " + this.viewableEmployee.getFirstName());
 		return empmgr.getListOfEmployees(this.viewableEmployee.getEmployeeID());
+	}
+	
+	public String assignValidator() {
+		int n = new Integer(newTimesheetValidator);
+		getViewableEmployee().setValidatorID(n);
+		empmgr.merge(viewableEmployee);
+		System.out.println("The new validator ID: " + n);		
+		return "viewMinions";
 	}
 	
 }
--- JavaSource/ca/bcit/infosys/models/ProjectEmployees.java
+++ JavaSource/ca/bcit/infosys/models/ProjectEmployees.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.Id;
-import javax.persistence.IdClass;
+//import javax.persistence.IdClass;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
@@ -14,6 +14,8 @@ import javax.persistence.Table;
 @Table(name="ProjectEmployees")
 //@IdClass(ProjectEmployeesKey.class)
 public class ProjectEmployees implements Serializable {
+
+	private static final long serialVersionUID = 1L;
 
 	//bi-directional one-to-many association to Employee
 	// This is the one side
