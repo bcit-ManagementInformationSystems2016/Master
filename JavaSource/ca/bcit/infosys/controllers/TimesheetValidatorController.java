a0423214300dee767081f5b77965adc95cdc039a
Brendan Voon <jh.voon@gmail.com>
2016-04-03 16:53:17 -0700

    added timesheet validator stuff

--- JavaSource/ca/bcit/infosys/controllers/TimesheetValidatorController.java
+++ JavaSource/ca/bcit/infosys/controllers/TimesheetValidatorController.java
@@ -19,49 +19,55 @@ public class TimesheetValidatorController implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
-	@PersistenceContext(unitName = "BluehostTesty")	EntityManager em;
-	
-	@Inject 
+	@PersistenceContext(unitName = "BluehostTesty")
+	EntityManager em;
+
+	@Inject
 	private EmployeeManager empmgr;
 	@Inject
 	private TimesheetManager tsmgr;
-	
+
 	// local variables
 	private Employee currentEmployee;
-	
+
 	// variables used for caching
 	private static Employee[] validatees;
 	private static Timesheet[] unapprovedTimesheets;
-	
+
 	// Getters and Setters
 	public Employee[] getValidatees() {
 		return validatees;
 	}
+
 	public void setValidatees(Employee[] validatees) {
 		TimesheetValidatorController.validatees = validatees;
 	}
+
 	public Employee getCurrentEmployee() {
 		return currentEmployee;
 	}
+
 	public void setCurrentEmployee(Employee currentEmployee) {
 		this.currentEmployee = currentEmployee;
 	}
+
 	public static Timesheet[] getUnapprovedTimesheets() {
 		return unapprovedTimesheets;
 	}
+
 	public static void setUnapprovedTimesheets(Timesheet[] unapprovedTimesheets) {
 		TimesheetValidatorController.unapprovedTimesheets = unapprovedTimesheets;
 	}
-	
+
 	// Other methods
-	
+
 	public Employee[] getYourValidatees() {
 		if (validatees == null) {
 			setValidatees(empmgr.getValidatees(Login.currentID));
 		}
 		return validatees;
 	}
-	
+
 	public Timesheet[] getYouUnapprovedTimesheets() {
 		if (validatees == null) {
 			setValidatees(empmgr.getValidatees(Login.currentID));
@@ -71,7 +77,7 @@ public class TimesheetValidatorController implements Serializable {
 		}
 		return unapprovedTimesheets;
 	}
-	
+
 	public String validateTimesheet(Timesheet ts) {
 		System.out.println("Validate Timesheet");
 		System.out.println("Employee ID: " + ts.getEmployeeID());
@@ -81,12 +87,24 @@ public class TimesheetValidatorController implements Serializable {
 		setValidatees(null);
 		return "getValidating";
 	}
-	
-	public String viewValidatees() {
-		setCurrentEmployee(empmgr.find(Login.currentID));
+
+	public String disapproveTimesheet(Timesheet ts) {
+		System.out.println("Disapprove Timesheet");
+		System.out.println("Employee ID: " + ts.getEmployeeID());
+		ts.setSubmitted(false);
+		tsmgr.merge(ts);
+		setUnapprovedTimesheets(null);
+		setValidatees(null);
 		return "getValidating";
 	}
-	
+
+	public String viewValidatees() {
+		setCurrentEmployee(empmgr.find(Login.currentID));
+		setUnapprovedTimesheets(null);
+		setValidatees(null);
+		return "getValidating";
+	}
+
 	public String leaveTimesheetValidating() {
 		setCurrentEmployee(null);
 		setUnapprovedTimesheets(null);
