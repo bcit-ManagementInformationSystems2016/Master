0204d0a7fd781da6ad505899c3896ca1c307118b
Brendan Voon <jh.voon@gmail.com>
2016-04-08 18:52:18 -0700

    disabled validator causing null pointer exception on edit employee

--- JavaSource/ca/bcit/infosys/controllers/HRValidator.java
+++ JavaSource/ca/bcit/infosys/controllers/HRValidator.java
@@ -10,7 +10,9 @@ import javax.faces.context.FacesContext;
 import javax.faces.validator.FacesValidator;
 import javax.faces.validator.Validator;
 import javax.faces.validator.ValidatorException;
+import javax.inject.Inject;
 
+import ca.bcit.infosys.managers.CredentialManager;
 import ca.bcit.infosys.models.Employee;
 
 /**
@@ -26,17 +28,22 @@ public class HRValidator implements Validator {
 		e = emp;
 	}
 
+	@Inject
+	CredentialManager crmgr;
+
 	@Override
 	public void validate(FacesContext facesContext, UIComponent component, Object value) throws ValidatorException {
 		String username = value.toString();
 
 		if (Login.map2.containsKey(username)) {
 			int ID = Login.map2.get(username);
-			if (ID != e.getEmployeeID()) {
-				FacesMessage msg = new FacesMessage("Username validation failed", "Username already exists");
-				msg.setSeverity(FacesMessage.SEVERITY_ERROR);
-				throw new ValidatorException(msg);
-			}
+			System.out.println(crmgr.getID(username));
+			// if (ID != crmgr.getID(username)) {
+			// FacesMessage msg = new FacesMessage("Username validation failed",
+			// "Username already exists");
+			// msg.setSeverity(FacesMessage.SEVERITY_ERROR);
+			// throw new ValidatorException(msg);
+			// }
 		}
 	}
 }
